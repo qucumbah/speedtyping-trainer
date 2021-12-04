4b1526f413b2839026ab0dddd481b2887c29a398
qucumbah <39967396+qucumbah@users.noreply.github.com>
2021-12-04 20:45:07 +0300

    dictionary generator: save only good definitions

--- dictionary-generator/index.js
+++ dictionary-generator/index.js
@@ -1,13 +1,40 @@
 const fs = require('fs');
+const path = require('path');
 
-const dictionary = JSON.parse(fs.readFileSync('./dictionary.json'));
-const definitions = Object.entries(dictionary);
+const dictionary = JSON.parse(fs.readFileSync(path.join(__dirname, './dictionary.json')));
+const definitions = Object.entries(dictionary).map(([word, description]) => `${word} - ${description}`.trim());
 
-let start = 0;
-let index = 0;
-while (start < definitions.length) {
-  const entriesChunk = definitions.slice(start, start + 1000);
-  fs.writeFileSync(`../src/words/batch${index}.ts`, 'export default ' + JSON.stringify(Object.fromEntries(entriesChunk)));
-  start += 1000;
-  index += 1;
+const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const uppercase = lowercase.toUpperCase();
+const numerals = '0123456789';
+const specials = '`~!@#$%^&*()-_=+[]{}\\|/,.<>? ;:\'"';
+
+const allowedCharacters = new Set([...lowercase, ...uppercase, ...numerals, ...specials]);
+function onlyContainsAllowedCharacters(definition) {
+  return Array.from(definition).every((character) => allowedCharacters.has(character));
+}
+
+function hasCorrectLength(definition) {
+  return definition.length >= 40 && definition.length <= 100;
+}
+
+const allowedDefinitions = definitions.filter((definition) => (
+  onlyContainsAllowedCharacters(definition)
+  && hasCorrectLength(definition)
+));
+
+const definitionsDir = path.resolve(__dirname, '../src/definitions');
+if (fs.existsSync(definitionsDir)) {
+  fs.rmSync(definitionsDir, { recursive: true });
+}
+fs.mkdirSync(definitionsDir);
+
+const batchSize = 1000;
+for (let batchNumber = 0; batchNumber * batchSize < allowedDefinitions.length; batchNumber += 1) {
+  const definitionsChunk = allowedDefinitions.slice(batchNumber * batchSize, (batchNumber + 1) * batchSize);
+
+  fs.writeFileSync(
+    path.join(definitionsDir, `batch${batchNumber}.ts`),
+    'export default ' + JSON.stringify(definitionsChunk, null, 2),
+  );
 }
